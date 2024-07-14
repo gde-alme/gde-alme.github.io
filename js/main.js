8ca6761a9add08d7559ae673ae387ce2ab4eeaac
gde-alme <goncalopes98@gmail.com>
2024-07-14 21:00:39 +0100

    block

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('nav a').click(function (e) {
-        e.preventDefault();
-        var page = $(this).data('page');
-        $('#content').load(page);
-    });
-
-    $('#content').load('pages/home.html');
-});
